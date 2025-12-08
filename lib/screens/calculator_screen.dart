b7ad6db6bb606f183023caa9946fd188f3f0571b
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-08 21:15:16 +0545

    first commit

---
+++ lib/screens/calculator_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+
+  final TextEditingController _controller = TextEditingController();
+  double first = 0;
+  double second =0;
+  String operator = "";
+  final List<String> characters = [
+    "C",
+    "*",
+    
+    "<-",
+    "1",
+    "2",
+    "3",
+    "+",
+    "4",
+    "5",
+    "6",
+    "-",
+    "7",
+    "8",
+    "9",
+    "*",
+    "%",
+    "0",
+    ".",
+    "=",
+  ];
+  final _globalKey = GlobalKey<FormState>();  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
